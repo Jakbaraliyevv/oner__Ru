16efe8d2b5f2a328438e6a8ff147709ac234972a
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2024-12-01 19:25:29 +0500

    Input ozgardi

--- src/components/showcase/index.jsx
+++ src/components/showcase/index.jsx
@@ -1,13 +1,10 @@
 import "./showcase.scss";
+import uzum from "../../img/bg__uzum.png";
 function Showcase() {
   return (
     <div className="showcase">
       <div className="container">
-        <img
-          src="https://images.uzum.uz/ct2au1jvgbkpg1nobsr0/main_page_banner.jpg"
-          alt="Image 1"
-          className="image"
-        />
+        <img src={uzum} alt="Image 1" className="image" />
       </div>
     </div>
   );
